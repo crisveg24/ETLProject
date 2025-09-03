3f74f7204df74673c86322cce9b9f48f0702a2d4
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2025-09-03 14:34:13 +0000

    Database updated

--- Config/config.py
+++ Config/config.py
@@ -3,4 +3,5 @@ class Config:
     Clase de configuración para rutas y parámetros del ETL.
     """
     INPUT_PATH = '/workspaces/ETLProject/Extract/Files/ncr_ride_bookings.csv'
-    OUTPUT_PATH = '/workspaces/ETLProject/Extract/Files/output_clean.csv'
+    SQLITE_DB_PATH = '/workspaces/ETLProject/Extract/Files/etl_data.db'
+    SQLITE_TABLE = 'ride_bookings_clean'
